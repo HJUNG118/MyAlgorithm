d0e1b3d9a2d7663098c58d3f0f36ba5e06ece74d
HJUNG118 <a38722243@gmail.com>
2023-08-09 16:35:05 +0900

    Feat: A와B12904 완료

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,11 @@
+for i in t:
+#     stack.append(i)
+#     if len(stack) <= len(s):
+#         continue
+#     else:
+#         a = s.append('A')
+#         if str(a) == str(stack):
+#           s.append('A')
+#         else:
+#           s.sort(reverse=True)
+#           s.append('B')
